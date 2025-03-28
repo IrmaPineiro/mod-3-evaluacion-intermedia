49ea78f128bbde3228cab223c8169de4a4b81218
Irmitate <irmitate@gmail.com>
2025-03-28 08:00:52 +0000

    structure

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import "../scss/App.scss";
+import ListCountries from "./ListCountries";
+import PropTypes from "prop-types";
+
+function App() {
+  return (
+    <>
+    <header>
+      <h1>Country Info App</h1>;
+    </header>
+    <main>
+      <ListCountry />
+    </main>
+  </>;
+  )
+}
+
+export default App;
---
+++ src/components/ListCountries.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContryCard from "./CountryCard";
+
+function ListCountries() {
+  return (
+    <div>
+      <CountryCard />
+    </div>
+  );
+}
+
+export default ListCountries;
